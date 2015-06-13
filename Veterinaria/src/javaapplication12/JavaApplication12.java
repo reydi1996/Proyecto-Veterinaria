67a525355cb48cffeb13462913fc138f21121c87
reydi1996 <a1410322@iberotec.edu.pe>
2015-06-13 10:46:24 -0500

    Hice un comentario
    para hacer el codigo

--- Veterinaria/src/javaapplication12/JavaApplication12.java
+++ Veterinaria/src/javaapplication12/JavaApplication12.java
@@ -15,7 +15,7 @@ public class JavaApplication12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Hay que avanzar el codigo
     }
     
 }
